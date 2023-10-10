25c099e1c3b3dfe7bbdb013ba3a668d0ee8275df
geon-youn <geon.youn@outlook.com>
2023-10-10 16:22:23 -0400

    change transition to be absolute position and increase size of logo at the end

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -4,10 +4,10 @@ function Transition({ hidden }) {
   return hidden ? (
     <></>
   ) : (
-    <>
+    <div className="transition">
       <div className="blur"></div>
       <img src="../../public/icon.png" alt="Geon osu! logo" />
-    </>
+    </div>
   );
 }
 
