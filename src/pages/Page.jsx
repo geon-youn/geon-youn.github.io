99356767859888440407006932780c4eb28db6e9
geon-youn <geon.youn@outlook.com>
2023-11-08 12:15:04 -0500

    remove unnecessary stylesheet

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,6 +1,5 @@
 import Transition from '../components/Transition';
 import Header from '../components/Header';
-import styles from '../styles/page.module.css';
 import { useEffect, useState } from 'react';
 
 function Page({ children, page = 'Home' }) {
@@ -22,7 +21,7 @@ function Page({ children, page = 'Home' }) {
       {showTransition ? <Transition page={page} /> : null}
 
       {/* header */}
-      {page === 'Home' ? null : <Header page={page}></Header>}
+      {page !== 'Home' ? <Header page={page}></Header> : null}
 
       {/* main content */}
       {children}
