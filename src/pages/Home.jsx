dd8597b099085841af75b9de12f33a4053fdd00d
geon-youn <geon.youn@outlook.com>
2023-11-09 15:43:07 -0500

    change home to use linkedcookie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Page from './Page';
-import Cookie from '../components/Cookie';
 import styles from '../styles/home.module.css';
+import LinkedCookie from '../components/LinkedCookie';
 
 function Home() {
   return (
     <Page name="Home">
       <div className={styles.home}>
-        <Cookie></Cookie>
+        <LinkedCookie></LinkedCookie>
       </div>
     </Page>
   );
