9f92cf3371a7f739f46b7e7a5e38509e79e8c2b3
geon-youn <geon.youn@outlook.com>
2023-11-09 15:42:47 -0500

    move cookie to its own component

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -1,62 +1,18 @@
-import Icon from '@mdi/react';
 import styles from '../styles/cookie.module.css';
-import links from '../common/links';
-import { useState } from 'react';
 
-function Cookie() {
-  // expand the cookie
-  const [expand, setExpand] = useState(false);
-
-  function handleClick() {
-    setExpand(!expand);
-  }
-
-  // set classes according to `expand`
-  // `original` is the original class name
-  function expandClasses(original) {
-    return `${styles[original]}${
-      expand ? '' : ' ' + styles[`${original}Hidden`]
-    }`;
-  }
-
+function Cookie({ onClick }) {
   return (
-    <div className={styles.cookie}>
-      <div className={styles.parentCookieImg} onClick={handleClick}>
-        {/* two images to create a nicer pulse effect */}
-        <img
-          className={styles.cookieImg}
-          src="/img/icon.png"
-          alt="Geon osu! logo"
-        />
-        <img
-          className={styles.ghostCookieImg}
-          src="/img/icon.png"
-          alt="Ghost of Geon osu! logo 1"
-        />
-      </div>
-      {/* parent div to create shadows */}
-      <div className={expandClasses('cookieLinks')}>
-        {/* child div clips links into a circle */}
-        <div className={styles.clip}>
-          {links.map((i) => {
-            return (
-              <a
-                key={i.text}
-                {...i.data}
-                className={styles.cookieLink}
-                onClick={i.text === 'Back' ? handleClick : null}
-              >
-                {/* link text */}
-                <div className={expandClasses('cookieLinkText')}>{i.text}</div>
-                {/* link svg */}
-                <div className={expandClasses('cookieLinkIcon')}>
-                  <Icon path={i.path} size={`min(5vw, 5vh)`} />
-                </div>
-              </a>
-            );
-          })}
-        </div>
-      </div>
+    <div className={styles.cookie} onClick={onClick}>
+      <img
+        className={styles.cookieMain}
+        src="/img/icon.png"
+        alt="Geon osu! logo"
+      />
+      <img
+        className={styles.cookieGhost}
+        src="/img/icon.png"
+        alt="Ghost of Geon osu! logo 1"
+      />
     </div>
   );
 }
