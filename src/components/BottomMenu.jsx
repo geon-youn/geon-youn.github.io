fed5593f66ad33e8fde60586b34b2a52e3cdac08
geon-youn <geon.youn@outlook.com>
2023-11-20 16:55:30 -0500

    add mode to bottom menu + css

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,10 +1,18 @@
 import styles from '../styles/bottommenu.module.css';
 import BackButton from './BackButton';
+import Mode from './Mode';
 
 function BottomMenu() {
-  return <footer className={styles.bottomMenu}>
-    <BackButton></BackButton>
-  </footer>;
+  return (
+    <footer className={styles.bottomMenu}>
+      <div className={styles.backButton}>
+        <BackButton></BackButton>
+      </div>
+      <div className={styles.modeButton}>
+        <Mode></Mode>
+      </div>
+    </footer>
+  );
 }
 
 export default BottomMenu;
