b1e452ace6ba3df273c97ac28c7d5a2cc98c9d67
geon-youn <geon.youn@outlook.com>
2023-11-20 14:45:26 -0500

    get rid of top menu

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -2,12 +2,10 @@ import Page from './Page';
 import Cookie from '../components/Cookie';
 import BottomMenu from '../components/BottomMenu';
 import styles from '../styles/play.module.css';
-import TopMenu from '../components/TopMenu';
 
 function Play() {
   return (
     <Page name="Play">
-      <TopMenu></TopMenu>
       <div className={styles.darkenBackground}></div>
       <BottomMenu></BottomMenu>
       <div className={styles.bottomLeftCookie}>
