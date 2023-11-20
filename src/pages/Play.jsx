1b26b72ad0ac12aa3f07dcc98a8c9cf362803e0e
geon-youn <geon.youn@outlook.com>
2023-11-20 13:51:41 -0500

    create skeleton for play section

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,7 +1,20 @@
 import Page from './Page';
+import Cookie from '../components/Cookie';
+import BottomMenu from '../components/BottomMenu';
+import styles from '../styles/play.module.css';
+import TopMenu from '../components/TopMenu';
 
 function Play() {
-  return <Page name="Play"></Page>;
+  return (
+    <Page name="Play">
+      <TopMenu></TopMenu>
+      <div className={styles.darkenBackground}></div>
+      <BottomMenu></BottomMenu>
+      <div className={styles.bottomLeftCookie}>
+        <Cookie></Cookie>
+      </div>
+    </Page>
+  );
 }
 
 export default Play;
