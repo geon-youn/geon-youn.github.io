b1c28ce193825da858eed348433674318f3ce2d8
geon-youn <geon.youn@outlook.com>
2023-11-20 16:55:55 -0500

    add mode context to play

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -2,15 +2,22 @@ import Page from './Page';
 import Cookie from '../components/Cookie';
 import BottomMenu from '../components/BottomMenu';
 import styles from '../styles/play.module.css';
+import { useState } from 'react';
+import ModeContext from '../contexts/ModeContext';
 
 function Play() {
+  const [mode, setMode] = useState(0);
+  const modeValue = { mode, setMode };
+
   return (
     <Page name="Play">
-      <div className={styles.darkenBackground}></div>
-      <BottomMenu></BottomMenu>
-      <div className={styles.bottomLeftCookie}>
-        <Cookie></Cookie>
-      </div>
+      <ModeContext.Provider value={modeValue}>
+        <div className={styles.darkenBackground}></div>
+        <BottomMenu></BottomMenu>
+        <div className={styles.bottomLeftCookie}>
+          <Cookie></Cookie>
+        </div>
+      </ModeContext.Provider>
     </Page>
   );
 }
