36bfce19aee1c69653e1b489c556c3d24d41f63e
geon-youn <geon.youn@outlook.com>
2023-11-25 11:35:42 -0500

    create factory for songs + add some entries for about mode

--- src/common/modes.js
+++ src/common/modes.js
@@ -16,3 +16,35 @@ export const modes = [
   mode('geon!skills', mdiAccountDetails),
   mode('geon!projects', mdiClipboardTextMultiple),
 ];
+
+const song = (title, author, mapper, difficulty, stars) => {
+  return { title, author, mapper, difficulty, stars };
+};
+
+export const modesData = [
+  [
+    song(
+      'B. Eng. in Computer Science',
+      'McMaster University',
+      'Sep. 2021 - Apr. 2024',
+      '3.9+ GPA',
+      7.21
+    ),
+    song(
+      'McMaster University Varsity Rowing',
+      'McMaster University',
+      'Sep. 2021 - Apr. 2024',
+      '',
+      6.9
+    ),
+    song(
+      'Full-Stack Web Developer',
+      'The Odin Project',
+      'Dec. 2022 - ongoing',
+      '',
+      8.5
+    )
+  ],
+  [],
+  [],
+];
